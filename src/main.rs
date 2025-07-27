b8dcd94fbd418c90a8cde026d680c4d80a99b256
fangpin <fpfangpin@hotmail.com>
2025-07-27 17:45:59 +0800

    minimal kernel

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,19 @@ fn panic(_info: &core::panic::PanicInfo) -> ! {
     loop {}
 }
 
+static HELLO: &[u8] = b"Hello, world!\n";
+
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
-    loop {}
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &c) in HELLO.iter().enumerate() {
+        unsafe {
+            // Each character takes 2 bytes: one for the character and one for the attribute
+            *vga_buffer.offset(i as isize * 2) = c; 
+            *vga_buffer.offset(i as isize * 2 + 1) = 0x0b; 
+        }
+    }
+
+    loop {} // Infinite loop to keep the program running
 }
