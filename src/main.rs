a350ffec661a881d7f77e228c962c40ccd2cf538
fangpin <fpfangpin@hotmail.com>
2025-07-27 21:17:12 +0800

    vga buffer

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 #![no_std]
 #![no_main]
 
+use os::vga_buffer;
+
 #[panic_handler]
 fn panic(_info: &core::panic::PanicInfo) -> ! {
     loop {}
@@ -10,15 +12,7 @@ static HELLO: &[u8] = b"Hello, world!\n";
 
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &c) in HELLO.iter().enumerate() {
-        unsafe {
-            // Each character takes 2 bytes: one for the character and one for the attribute
-            *vga_buffer.offset(i as isize * 2) = c; 
-            *vga_buffer.offset(i as isize * 2 + 1) = 0x0b; 
-        }
-    }
+    vga_buffer::write_something();
 
     loop {} // Infinite loop to keep the program running
 }
